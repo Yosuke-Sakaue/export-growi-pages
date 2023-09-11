d701b915779039f914c0c01efdac8bd81120f0d2
Yosuke-Sakaue <Yosuke_Sakaue@sis2.saison.co.jp>
2023-09-11 21:59:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Summary
+入力されたGrowiパス配下のmdファイルを抽出し、パスの構成を再帰的にoutputディレクトリに出力するスクリプト
+
+## Usage
+1. create .env file
+1. pip install python-dotenv
+1. python main.py <GROWIのパス（例：/モダンインフラ/導入事例）>
+
+## Requarements Env
+```
+GROWI_BASE_URL
+GROWI_API_TOKEN
+```
+https://tips.weseek.co.jp/5e4d5253340a4f0049473d2b
---
+++ main.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import requests
+import json
+from dotenv import load_dotenv
+load_dotenv()  
+
+class GrowiAPIError(Exception):
+    def __init__(self, description):
+        super().__init__()
+        self.description = description
+    
+    def __repr__(self):
+        return str(self.description)
+
+    def __str__(self):
+        return str(self.description)
+
+def get_page_list(base_url: str, api_token: str, page_path: str) -> dict:
+    """
+    get page information
+    """
+    req_url = '{}{}'.format(base_url, '/_api/pages.list')
+    params={'access_token': f'{api_token}', 'path': page_path}
+    res = requests.get(req_url, params=params)
+
+    if res.status_code == 200:
+        return json.loads(res.text)['pages']
+    else:
+        raise GrowiAPIError(res.text)
+    
+def get_page_by_id(base_url: str, api_token: str, page_id: str) -> dict:
+    """
+    get page information
+    """
+    req_url = '{}{}'.format(base_url, '/_api/v3/page')
+    params={'access_token': f'{api_token}', 'pageId': page_id}
+    res = requests.get(req_url, params=params)
+
+    if res.status_code == 200:
+        return json.loads(res.text)['page']
+    else:   
+        raise GrowiAPIError(res.text)
+    
+def create_md_file(page_info: dict) -> None:
+    """
+    create folder and md file
+    """
+    os.makedirs('output' + "/".join(page_info['path'].split("/")[:-1]), exist_ok=True)
+    if not page_info['revision']['body'] == '':
+        with open('output' + page_info['path'] + '.md', "w", encoding="utf-8") as f:
+            f.write(page_info['revision']['body'])
+        
+    
+if __name__ == '__main__':
+    base_url = os.getenv("GROWI_BASE_URL")
+    api_token = os.getenv("GROWI_API_TOKEN")
+    path = sys.argv[1]
+
+    pages = get_page_list(base_url, api_token, path)
+
+    for page in pages:
+        page_info = get_page_by_id(base_url, api_token, page['_id'])
+        create_md_file(page_info)
+        
+    print("OK")
